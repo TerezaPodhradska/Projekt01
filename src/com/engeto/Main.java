c1b104ca96986162edf62f8dee84cff0778b155a
TerezaPodhradska <podhradskaterezaa@gmail.com>
2022-03-20 11:49:47 +0100

    VAT over 20 a přímo určené

--- src/com/engeto/Main.java
+++ src/com/engeto/Main.java
@@ -4,6 +4,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static final String FILENAME = "vat–eu.csv";
@@ -15,12 +16,10 @@ public class Main {
         Loading loading = new Loading();
         try {
             loading.loadFromFile(FILENAME, DELIMITER);
-
         } catch (Exception e){
             System.out.println("Špatný soubot k nahrání!"+ e.getLocalizedMessage());
         }
         System.out.println("Název země (zkratka): základní sazba %" );
-
         for (State country : loading.getAllCountries()) {
             System.out.println(country.getDescription());
         }
@@ -29,10 +28,9 @@ public class Main {
         System.out.println("================================================");
         System.out.println("Sazba VAT 20+% a nepoužívají specialní sazbu");
         loading.compareTo();
-        List<String> statesOver20 = new ArrayList<String>();
+        List<String> statesOver20 = new ArrayList<>();
         for (State country : loading.getAllCountries()) {
-
-            if ((country.getFullTAxValue() > 20)&& (country.isSpecialRate() == false)) {
+            if ((country.getFullTAxValue() > 20)&& (!country.isSpecialRate())) {
                 System.out.println(country.getDescriptionOver20());
                 statesOver20.add(country.getDescriptionOver20());
             }
@@ -42,7 +40,7 @@ public class Main {
         statesOver20.add("Sazba VAT nižší než 20% nebo používají speciální sazbu: ");
         System.out.println("Sazba VAT 20 % nebo nižší nebo používají speciální sazbu: ");
         for (State country : loading.getAllCountries()) {
-            if ((country.getFullTAxValue() <= 20) && ( country.isSpecialRate()== true)) {
+            if ((country.getFullTAxValue() <= 20) && country.isSpecialRate()) {
                 System.out.println(country.getDescriptionUndere20());
                 statesOver20.add(country.getDescriptionUndere20());
             }
@@ -50,10 +48,39 @@ public class Main {
 
         //nahraje výpis do souboru
         FileWriter writer = new FileWriter("vat-over-20.txt");
-        for(String str: statesOver20) {
-            writer.write(str + System.lineSeparator());
+        for(String state: statesOver20) {
+            writer.write(state + System.lineSeparator());
         }
         writer.close();
 
+
+
+        // hodnoty s určenou hranicí minimalního VAT
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Zadejte minimalní hodnotu VAT: ");
+        String minVatValueString = scanner.nextLine();
+        int minVatValue;
+        if(minVatValueString.equals("")){
+            minVatValue = 20;
+        } else {
+            minVatValue = Integer.parseInt(minVatValueString);
+        }
+        System.out.println("Sazba VAT " + minVatValue + "+% : ");
+        loading.compareTo();
+        List<String> statesOverX = new ArrayList<String>();
+        for (State country : loading.getAllCountries()) {
+            if ((country.getFullTAxValue() > minVatValue)) {
+                System.out.println(country.getDescriptionOver20());
+                statesOverX.add(country.getDescriptionOver20());
+            }
+        }
+        //nahraje výpis do nového souboru
+        String fileXName = "vat-over-"+minVatValueString+".txt";
+        FileWriter writerX = new FileWriter(fileXName);
+        for(String state: statesOverX) {
+            writerX.write(state + System.lineSeparator());
+        }
+        writerX.close();
+        System.out.println("Jméno souboru, ve kterém se státy nacházejí: "+fileXName);
     }
 }
